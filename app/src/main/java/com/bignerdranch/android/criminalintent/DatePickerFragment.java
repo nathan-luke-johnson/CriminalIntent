ff960896f485fa0242a247f5d2449bc7f2897c0f
Luke Johnson <lukej@flowjo.com>
2016-11-05 17:28:16 -0700

    It works now, except for 2.7 inch screens. And who cares?

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -25,6 +27,7 @@ public class DatePickerFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
 
     private DatePicker mDatePicker;
+    private TimePicker mTimePicker;
 
     public static DatePickerFragment newInstance(Date date) {
         Bundle args = new Bundle();
@@ -44,23 +47,49 @@ public class DatePickerFragment extends DialogFragment {
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH);
         int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
 
-        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date_time, null);
 
         mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_date_picker);
         mDatePicker.init(year, month, day, null);
+
+        mTimePicker = (TimePicker) v.findViewById(R.id.dialog_date_time_picker);
+        if (Build.VERSION.SDK_INT < 23) {
+            mTimePicker.setCurrentHour(hour);
+            mTimePicker.setCurrentMinute(minute);
+        } else {
+            mTimePicker.setHour(hour);
+            mTimePicker.setMinute(minute);
+        }
+        mTimePicker.setIs24HourView(false);
 
         return new AlertDialog.Builder(getActivity())
                 .setView(v)
                 .setTitle(R.string.date_picker_title)
                 //.setPositiveButton(android.R.string.ok, null)
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        sendResult(Activity.RESULT_CANCELED, null);
+                    }
+                })
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         int year = mDatePicker.getYear();
                         int month = mDatePicker.getMonth();
                         int day = mDatePicker.getDayOfMonth();
-                        Date date = new GregorianCalendar(year, month, day).getTime();
+                        int hour; int minute;
+                        if(Build.VERSION.SDK_INT < 23) {
+                            hour = mTimePicker.getCurrentHour();
+                            minute = mTimePicker.getCurrentMinute();
+                        } else {
+                            hour = mTimePicker.getHour();
+                            minute = mTimePicker.getMinute();
+                        }
+                        Date date = new GregorianCalendar(year, month, day, hour, minute).getTime();
                         sendResult(Activity.RESULT_OK, date);
                     }
                 })
